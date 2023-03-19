2739e56e2d3b5df97406f48635ecf4b697a43ed2
+ <+>
2023-03-19 17:46:46 +0300

    done

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,37 @@
+class SpicesController < ApplicationController
+     # GET /spices
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+#   # GET /spices/:id
+#   def show
+#     spice = Spice.find_by(id: params[:id])
+#     render json: spice
+#   end
+
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    spice.update(spice_params)
+    render json: spice
+  end
+  
+   def destroy
+    spice = Spice.find_by(id: params[:id])
+    spice.destroy
+    head :no_content 
+   end
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+end
